4ac564282596b063833ce24633940be6f094c743
sajim-khan <shossain1427@gmail.com>
2023-07-26 23:32:35 +0600

    added random password generator

--- assignment.js
+++ assignment.js
@@ -14,7 +14,6 @@ function reverseStr(string) {
 
 
 
-
 //Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array.
 
 //Example Input: [2, -5, 10, -3, 7] Example Output: 19
@@ -61,6 +60,7 @@ function findMostFrequentElement(arr) {
 // console.log(`Most Frequent Element is : ${result}`); // Output: 3
 
 
+
 // Task 4: Create a function that takes a sorted array of numbers and a target value as input. The function should find two numbers in the array that add up to the target value. Return an array containing the indices of the two numbers.
 
 //Example Input: ([1, 3, 6, 8, 11, 15], 9) Example Output: [1, 2] (numbers at indices 1 and 2: 3 + 6 = 9)
@@ -102,7 +102,6 @@ function calculator(num1, operator, num2) {
   }
 }
 
-
 const add = calculator(10, '+', 10);
 const sub = calculator(10, '-', 5);
 const mul = calculator(10, '*', 10);
@@ -111,3 +110,26 @@ console.log(`Add : ${add},  Sub : ${sub},  Mul : ${mul},  Divide : ${divide}`);
 
 
 
+// Task 6: Create a program that generates a random password of a specified length. The password should include a mix of uppercase letters, lowercase letters, numbers, and special characters.
+
+
+const generateRandomPass = (length) => {
+  const uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+  const numericChar = "0123456789";
+  const specialChar = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+  const allChar =
+    uppercaseChar + lowercaseChar + numericChar + specialChar;
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * allChar.length);
+    password += allChar[randomIndex];
+  }
+
+  return password;
+};
+
+const randomPassword = generateRandomPass(8);
+console.log(randomPassword);
