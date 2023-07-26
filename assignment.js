3d5b11c379dd57c5e97146b146d405958482575e
sajim-khan <shossain1427@gmail.com>
2023-07-26 22:00:50 +0600

    added returns the sum of all positive numbers from an array

--- assignment.js
+++ assignment.js
@@ -2,12 +2,40 @@
 
 //Example Input: "hello world" Example Output: "dlrow olleh"
 
-function reverseStr(string) {
-  let reversedStr = ""
-  const myStr = string.toString().toLowerCase();
-  for (let i = string.length - 1; i >=0; i--){
-    reversedStr+=myStr[i]
+// function reverseStr(string) {
+//   let reversedStr = ""
+//   const myStr = string.toString().toLowerCase();
+//   for (let i = string.length - 1; i >=0; i--){
+//     reversedStr+=myStr[i]
+//   }
+//   console.log(`Input: ${myStr} \nOutput: ${reversedStr} `);
+// }
+// reverseStr("Hello World") //Output: dlrow olleh 
+
+
+
+
+//Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array.
+
+//Example Input: [2, -5, 10, -3, 7] Example Output: 19
+
+function sumOfPositiveNum(array) {
+  let myArray = array.length;
+  let sum = 0;
+  let positiveArray = [];
+  for (let i = 0; i < myArray; i++) {
+    if (i % 2 === 0) {
+      sum += array[i];
+      positiveArray.push(array[i]);
+    }
   }
-  console.log(`Input: ${myStr} \nOutput: ${reversedStr} `);
+  console.log(
+    `Input Array: [${array}] \nPositive number is : ${positiveArray} \nSum of all positive numbers is : ${sum} `
+  );
 }
-reverseStr("Hello World") //Output: dlrow olleh 
+sumOfPositiveNum([2, -5, 10, -3, 7]);
+
+
+
+
+
