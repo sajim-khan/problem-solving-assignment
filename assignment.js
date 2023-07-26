d8ac113b874286e1c21f2b5118a5c77dc7750bab
sajim-khan <shossain1427@gmail.com>
2023-07-26 23:15:14 +0600

    added  Return an array containing the indices of the two numbers

--- assignment.js
+++ assignment.js
@@ -57,5 +57,31 @@ function findMostFrequentElement(arr) {
   return mostFrequentElement;
 }
 
-const result = findMostFrequentElement([3, 5, 2, 5, 3, 3, 1, 4, 5]);
-console.log(`Most Frequent Element is : ${result}`); // Output: 3
+// const result = findMostFrequentElement([3, 5, 2, 5, 3, 3, 1, 4, 5]);
+// console.log(`Most Frequent Element is : ${result}`); // Output: 3
+
+
+// Task 4: Create a function that takes a sorted array of numbers and a target value as input. The function should find two numbers in the array that add up to the target value. Return an array containing the indices of the two numbers.
+
+//Example Input: ([1, 3, 6, 8, 11, 15], 9) Example Output: [1, 2] (numbers at indices 1 and 2: 3 + 6 = 9)
+
+function findTwoNumbersWithSum(sortedArray, targetValue) {
+  const numToIndexMap = {};
+
+  for (let i = 0; i < sortedArray.length; i++) {
+    const complement = targetValue - sortedArray[i];
+
+    if (complement in numToIndexMap) {
+      return [numToIndexMap[complement], i];
+    }
+
+    numToIndexMap[sortedArray[i]] = i;
+  }
+
+  return [];
+}
+
+const result = findTwoNumbersWithSum([1, 3, 6, 8, 11, 15], 9);
+console.log("Indices of the two numbers:", result);
+
+
